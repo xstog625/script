7b50e27f5fc2e31feafb8062dbbc4474cabc505c
Zarif Arzimetov <zarif.arzimetov@gmail.com>
2023-12-15 15:50:23 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,4 +30,4 @@ if [ "${#config_args[@]}" -eq 0 ]; then
     exit 1
 fi
 
-semgrep ci --exclude=semgrep-rules/ --metrics=off "${config_args[@]}" --sarif -o results.sarif >> semgrep-output.txt
+semgrep ci --config semgrep-rules/python --sarif -o results.sarif >> semgrep-output.txt
